34f8e1daf56c10fac510b58a7435fea7a31debbe
gongyi <gxylogin@sina.com>
2019-05-02 15:15:30 +0800

    按奇偶数排序数组

--- 按奇偶排序数组.py
+++ 按奇偶排序数组.py
@@ -10,7 +10,7 @@ def sortArrayByParity(A):
     '''
     N = len(A)
     result = ['']*N
-    i,j = 0,1
+    i, j = 0, 1
     for num in A:
         if num % 2 == 0:
             # 是偶数
@@ -22,16 +22,17 @@ def sortArrayByParity(A):
             j += 2
     return result
 
+
 def sortArrayByParity1(A):
     '''
     python高赞解法，利用了zip函数
     '''
-    #先使用列表生成器生成两个列表，分别是偶数和奇数
+    # 先使用列表生成器生成两个列表，分别是偶数和奇数
     ou = [i for i in A if i % 2]
     ji = [i for i in A if not i % 2]
     # 利用zip函数将两个列表ou和ji合并成一个个元组，每个元组中一个偶数一个奇数
     # 再遍历元组列表，将元组列表中的数字一一取出，构成最终列表返回
-    return [i for n in zip(ji,ou) for i in n]
+    return [i for n in zip(ji, ou) for i in n]
 
 # 测试
 A = [4, 2, 5, 7]  # output：[4,5,2,7]或者[4,7,2,5]或者[2,5,4,7]或者[2,7,4,5]
